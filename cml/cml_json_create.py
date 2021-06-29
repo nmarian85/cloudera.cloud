0d98702d5592f743dec857b84210631e8f0076b1
Nicolaie.Marian <nicolaie.marian@ecb.europa.eu>
2021-06-29 21:29:55 +0200

    separated build and run time scripts

--- cml/cml_json_create.py
+++ cml/cml_json_create.py
@@ -31,6 +31,8 @@ all_subnets = [i for i in os.getenv("ALL_SUBNETS").split(" ")]
 
 # read skeleton from command cdp ml create-workspace --generate-cli-skeleton
 cml_json = json.load(sys.stdin)
+print(json.dumps(cml_json, indent=4, sort_keys=True))
+
 cml_json["environmentName"] = env_name
 cml_json["workspaceName"] = workspace_name
 cml_json["usePublicLoadBalancer"] = False
