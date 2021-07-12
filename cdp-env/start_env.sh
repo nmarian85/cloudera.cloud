837059e3fb14b67df7265de76016aedb322dce1a
Nicolaie.Marian <nicolaie.marian@ecb.europa.eu>
2021-07-12 21:05:00 +0200

    still trying cml

---
+++ cdp-env/start_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eo pipefail
+set -o errexit
+set -o nounset
+
+while true; 
+do
+    OUT=$(cdp environments describe-environment --environment-name $ENV_NAME)
+    echo $OUT
+    STATUS=$(echo $OUT | python -c 'import json,sys; print(json.load(sys.stdin)["environment"]["status"])')
+    [ $STATUS == 'AVAILABLE' ] && exit 0
+done
