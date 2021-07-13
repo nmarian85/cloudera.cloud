670d8fef40e23511f4da921aa33fba65f492db47
Nicolaie.Marian <nicolaie.marian@ecb.europa.eu>
2021-07-13 16:42:46 +0200

    still trying cml

--- cdp-env/wait_for_status.sh
+++ cdp-env/wait_for_status.sh
@@ -4,10 +4,11 @@ set -eo pipefail
 set -o errexit
 set -o nounset
 
+EXPECTED_STATUS=$1
 while true; 
 do
     OUT=$(cdp environments describe-environment --environment-name $ENV_NAME)
     # echo $OUT
     STATUS=$(echo $OUT | python -c 'import json,sys; print(json.load(sys.stdin)["environment"]["status"])')
-    [ $STATUS == 'AVAILABLE' ] && exit 0
+    [ $STATUS == $EXPECTED_STATUS ] && exit 0
 done
