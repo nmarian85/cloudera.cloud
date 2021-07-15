c043cc84f97b6cc1ea4821b32eea8c6d452d9ad1
Nicolaie.Marian <nicolaie.marian@ecb.europa.eu>
2021-07-15 15:15:57 +0200

    waiting for actions

--- wait_for_status.sh
+++ wait_for_status.sh
@@ -4,11 +4,26 @@ set -eo pipefail
 set -o errexit
 set -o nounset
 
-EXPECTED_STATUS=$1
+COMPONENT=$1
+EXPECTED_STATUS=$2
+STATUS=""
 while true; 
 do
-    OUT=$(cdp environments describe-environment --environment-name $ENV_NAME)
-    # echo $OUT
-    STATUS=$(echo $OUT | python -c 'import json,sys; print(json.load(sys.stdin)["environment"]["status"])')
+    case $COMPONENT in
+        CML)
+            OUT=$(cdp ml describe-workspace --workspace-name $CML_CLUSTER_NAME --environment-name $ENV_NAME 2>/dev/null)
+            # this check is to be used to verify if the workspace was already deleted
+            [ -z $OUT ] && exit 0
+            STATUS=$(echo $OUT | python -c 'import json,sys; print(json.load(sys.stdin)["workspace"]["instanceStatus"])')
+        ;;
+        CDE)
+        ;;
+        ENV)
+            OUT=$(cdp environments describe-environment --environment-name $ENV_NAME)
+            # this check is to be used to verify if the environment was already deleted as part of the delete workflow
+            [ -z $OUT ] && exit 0
+            STATUS=$(echo $OUT | python -c 'import json,sys; print(json.load(sys.stdin)["environment"]["status"])')
+        ;;
+    
     [ $STATUS == $EXPECTED_STATUS ] && exit 0
 done
