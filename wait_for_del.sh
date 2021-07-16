ff26e24ebd7677c9bff389c7ebe5c4a83f64ea7f
Nicolaie.Marian <nicolaie.marian@ecb.europa.eu>
2021-07-16 12:29:31 +0200

    cde work

--- wait_for_del.sh
+++ wait_for_del.sh
@@ -9,12 +9,12 @@ while true;
 do
     case $COMPONENT in
         CML)
-            cdp ml describe-workspace --workspace-name $CML_CLUSTER_NAME --environment-name $ENV_NAME || exit 0 
+            cdp ml describe-workspace --workspace-name $CML_CLUSTER_NAME --environment-name $ENV_NAME  > /dev/null 2>&1 || exit 0 
         ;;
         CDE)
         ;;
         ENV)
-            cdp environments describe-environment --environment-name $ENV_NAME || exit 0
+            cdp environments describe-environment --environment-name $ENV_NAME > /dev/null 2>&1 || exit 0
         ;;
     esac
 done
