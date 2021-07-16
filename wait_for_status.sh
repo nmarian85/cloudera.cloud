a8708cf99a4833614a47e9aaf37aee67dabb580b
Nicolaie.Marian <nicolaie.marian@ecb.europa.eu>
2021-07-16 10:33:05 +0200

    waiting for actions

--- wait_for_status.sh
+++ wait_for_status.sh
@@ -13,7 +13,7 @@ do
         CML)
             OUT=$(cdp ml describe-workspace --workspace-name $CML_CLUSTER_NAME --environment-name $ENV_NAME 2>/dev/null)
             # this check is to be used to verify if the workspace was already deleted
-            [ -z $OUT ] && exit 0
+            [ -z "$OUT" ] && exit 0
             STATUS=$(echo $OUT | python -c 'import json,sys; print(json.load(sys.stdin)["workspace"]["instanceStatus"])')
         ;;
         CDE)
@@ -21,7 +21,7 @@ do
         ENV)
             OUT=$(cdp environments describe-environment --environment-name $ENV_NAME)
             # this check is to be used to verify if the environment was already deleted as part of the delete workflow
-            [ -z $OUT ] && exit 0
+            [ -z "$OUT" ] && exit 0
             STATUS=$(echo $OUT | python -c 'import json,sys; print(json.load(sys.stdin)["environment"]["status"])')
         ;;
     esac
