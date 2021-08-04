596c5b681eb8128a3d1cab738b86d8b7f8453cb7
Nicolaie.Marian <nicolaie.marian@ecb.europa.eu>
2021-08-04 16:44:43 +0200

    redesign: credentials

--- scripts/dump_cred_json.py
+++ scripts/dump_cred_json.py
@@ -33,7 +33,7 @@ for cred, cred_info in credentials.items():
     cred_json["credentialName"] = cred_info["credential_name"]
     cred_json["roleArn"] = cred_info["role_arn"]
     cred_json["description"] = cred_info["description"]
-    with open(f"{cred_info.credential_name}_cred.json", "w", encoding="utf-8") as f:
+    with open(f'{cred_info["credential_name"]}_cred.json', "w", encoding="utf-8") as f:
         json.dump(cred_json, f, ensure_ascii=False, indent=4)
 
 # cml_cluster = envs[cdp_env_name]["cml_clusters"][cluster_name]
