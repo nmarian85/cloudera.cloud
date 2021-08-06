b1fa669c0b52dab28c07e4c71f4f71dae3a157c5
Nicolaie.Marian <nicolaie.marian@ecb.europa.eu>
2021-08-06 16:36:58 +0200

    added dlake creation

--- scripts/dump_dlake_json.py
+++ scripts/dump_dlake_json.py
@@ -56,6 +56,6 @@ with open(f'{dlake_name}.json', "w", encoding="utf-8") as f:
 
 print(
     f'''{dlake_name},'''
-    f'''{role_iam_arn}:role/{cdp_dl_json["ranger_role"]},'''
-    f'''{role_iam_arn}:role/{cdp_dl_json["data_role"]}')'''
+    f'''{role_iam_arn}:role/{cdp_dl_info["ranger_role"]},'''
+    f'''{role_iam_arn}:role/{cdp_dl_info["data_role"]}')'''
 )
