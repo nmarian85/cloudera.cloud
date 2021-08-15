f9b8e770b910eac178402b7e6c38968c49eb6dc7
Nicolaie.Marian <nicolaie.marian@ecb.europa.eu>
2021-08-15 09:45:03 +0200

    added delete vc cde

--- scripts/dump_cde_vc_json.py
+++ scripts/dump_cde_vc_json.py
@@ -2,11 +2,8 @@ import sys
 import json
 import os
 import argparse
-
-def get_vc_id(all_vcs, vc_name):
-    for vc in all_vcs["vcs"]:
-        if vc["vcName"] == vc_name:
-            return vc["vcId"]
+import env_info
+import cde_helper
 
 my_parser = argparse.ArgumentParser(description="Install or delete CDE virtual clusters")
 
@@ -14,64 +11,107 @@ my_parser = argparse.ArgumentParser(description="Install or delete CDE virtual c
 my_parser.add_argument(
     "--action", choices=["install", "delete"], help="Install or delete CDE cluster",
 )
-my_parser.add_argument("-e", "--env", help="Environment (e.g. lab)")
-my_parser.add_argument("-c", "--cdp-env-name", help="CDP env name")
-my_parser.add_argument("-m", "--cdecluster", help="CDE cluster name")
-my_parser.add_argument("-i", "--clusterid", help="CDE cluster id")
-
-# if defined, only this VC will be installed/deleted, otherwise all VCs belonging
-# to the CDE cluster will be
 my_parser.add_argument("-v", "--cdevccluster", action='store_true', help="VC CDE cluster name")
 
 args = my_parser.parse_args()
 
-cdp_env_name = args.cdp_env_name
-env = args.env
-cluster_name = args.cdecluster
-cluster_id = args.clusterid
-
-with open(f"{env}.json", "r") as read_file:
-    envs = json.load(read_file)
-
-if envs.get(cdp_env_name) is None:
-    raise ValueError(f"Unable to find {cdp_env_name} in env.json")
-
-with open("skel.json") as json_file:
-    env_json_skel = json.load(json_file)
-
-cdp_env_info = envs.get(cdp_env_name)
-cde_cluster = cdp_env_info.get("cde_clusters").get(cluster_name)
-
-if cde_cluster is None:
-    raise ValueError(f"Unable to find {cluster_name} in env.json")
-
 with open("skel.json") as json_file:
     cde_vc_json_skel = json.load(json_file)
 
-# we target only a specific vcs if the vc was specified in the CLI
-if args.cdevccluster:
-    vcs = {args.cdevccluster: cde_cluster["vcs"][args.cdevccluster]}
-else:
-    vcs = cde_cluster["vcs"]
+for cluster_name in os.getenv("CDE_CLUSTERS"):
+    cde_cluster = env_info.cdp_env_info["cde_clusters"][cluster_name]
 
-if args.action == "delete":
-    with open("vcs.json") as json_vcs_file:
-        all_vcs = json.load(json_vcs_file)
+    if cde_cluster is None:
+        raise ValueError(f"Unable to find {cluster_name} in env.json")
 
-for vc_name, vc_info in vcs.items():
-    cde_vc_json = dict(cde_vc_json_skel)
-    cde_vc_json["clusterId"] = cluster_id
+    if args.cdevccluster:
+        vcs = {args.cdevccluster: cde_cluster["vcs"][args.cdevccluster]}
+    else:
+        vcs = cde_cluster["vcs"]
+
     if args.action == "install":
-        cde_vc_json["name"] = vc_name
-        cde_vc_json["cpuRequests"] = vc_info["cpu_requests"]
-        cde_vc_json["memoryRequests"] = vc_info["memory_requests"]
-        cde_vc_json["chartValueOverrides"] = vc_info["chart_value_overrides"]
-        rsc = vc_info["runtime_spot_component"]
-        if rsc == "DEFAULT":
-            del cde_vc_json["runtimeSpotComponent"]
-        else:
-            cde_vc_json["runtimeSpotComponent"] = rsc
-    elif args.action == "delete":
-        cde_vc_json["vcId"] = get_vc_id(all_vcs, vc_name)
-    with open(f"{vc_name}_vc.json", "w", encoding="utf-8") as f:
-        json.dump(cde_vc_json, f, ensure_ascii=False, indent=4)
+        for vc_name, vc_info in vcs.items():
+            cde_vc_json = dict(cde_vc_json_skel)
+            cde_vc_json["clusterId"] = cde_helper.get_cluster_id(cluster_name)
+            cde_vc_json["name"] = vc_name
+            cde_vc_json["cpuRequests"] = vc_info["cpu_requests"]
+            cde_vc_json["memoryRequests"] = vc_info["memory_requests"]
+            cde_vc_json["chartValueOverrides"] = vc_info["chart_value_overrides"]
+            rsc = vc_info["runtime_spot_component"]
+            if rsc == "DEFAULT":
+                del cde_vc_json["runtimeSpotComponent"]
+            with open(f"{vc_name}_vc.json", "w", encoding="utf-8") as f:
+                json.dump(cde_vc_json, f, ensure_ascii=False, indent=4)
+
+
+# my_parser.add_argument("-e", "--env", help="Environment (e.g. lab)")
+# my_parser.add_argument("-c", "--cdp-env-name", help="CDP env name")
+# my_parser.add_argument("-m", "--cdecluster", help="CDE cluster name")
+# my_parser.add_argument("-i", "--cdeclusterid", help="CDE cluster id")
+
+# # if defined, only this VC will be installed/deleted, otherwise all VCs belonging
+# # to the CDE cluster will be
+# my_parser.add_argument("-v", "--cdevccluster", action='store_true', help="VC CDE cluster name")
+
+# args = my_parser.parse_args()
+
+# cdp_env_name = args.cdp_env_name
+# env = args.env
+# cluster_name = args.cdecluster
+# cluster_id = args.cdeclusterid
+
+# with open(f"{env}.json", "r") as read_file:
+#     envs = json.load(read_file)
+
+# if envs.get(cdp_env_name) is None:
+#     raise ValueError(f"Unable to find {cdp_env_name} in env.json")
+
+# with open("skel.json") as json_file:
+#     env_json_skel = json.load(json_file)
+
+# cdp_env_info = envs.get(cdp_env_name)
+# cde_cluster = cdp_env_info.get("cde_clusters").get(cluster_name)
+
+# if cde_cluster is None:
+#     raise ValueError(f"Unable to find {cluster_name} in env.json")
+
+# with open("skel.json") as json_file:
+#     cde_vc_json_skel = json.load(json_file)
+
+# # we target only a specific vcs if the vc was specified in the CLI
+# if args.cdevccluster:
+#     vcs = {args.cdevccluster: cde_cluster["vcs"][args.cdevccluster]}
+# else:
+#     vcs = cde_cluster["vcs"]
+
+# if args.action == "delete":
+#     with open("vcs.json") as json_vcs_file:
+#         all_vcs = json.load(json_vcs_file)
+#     # we need this list to know which virtual clusters to delete
+#     all_vcid = []
+
+# for vc_name, vc_info in vcs.items():
+#     cde_vc_json = dict(cde_vc_json_skel)
+#     cde_vc_json["clusterId"] = cluster_id
+#     if args.action == "install":
+#         cde_vc_json["name"] = vc_name
+#         cde_vc_json["cpuRequests"] = vc_info["cpu_requests"]
+#         cde_vc_json["memoryRequests"] = vc_info["memory_requests"]
+#         cde_vc_json["chartValueOverrides"] = vc_info["chart_value_overrides"]
+#         rsc = vc_info["runtime_spot_component"]
+#         if rsc == "DEFAULT":
+#             del cde_vc_json["runtimeSpotComponent"]
+#         else:
+#             cde_vc_json["runtimeSpotComponent"] = rsc
+#     elif args.action == "delete":
+#         vc_id = get_vc_id(all_vcs, vc_name)
+#         cde_vc_json["vcId"] = vc_id
+#         all_vcid.append(vc_id)
+#     with open(f"{vc_name}_vc.json", "w", encoding="utf-8") as f:
+#         json.dump(cde_vc_json, f, ensure_ascii=False, indent=4)
+
+# # writing the vcids to a csv file in order to be able to wait for their deletion to finish outside the script
+# if args.action == "delete":
+#     with open(f"{cluster_name}.csv", "w", encoding="utf-8") as f:
+#         for vcid in all_vcid:
+#             f.write(f"{vcid},")
--- scripts/wait_for_del.sh
+++ scripts/wait_for_del.sh
@@ -6,17 +6,17 @@ set -o nounset
 
 COMPONENT=$1
 
-if [ "$COMPONENT" = "CDE" ]; then
-    cat << EOF > get_cluster_id.py
-import json,sys,os
-services=json.load(sys.stdin).get("services")
-for service in services:
-    if service.get("name") == os.getenv("CDE_CLUSTER_NAME"):
-        print(service.get("clusterId"))
-EOF
-    CLUSTER_ID=$(cdp de list-services | python get_cluster_id.py)
-    [ ! -z "$CLUSTER_ID" ] && cdp de disable-service --cluster-id $CLUSTER_ID
-fi
+# if [ "$COMPONENT" = "CDE" ]; then
+#     cat << EOF > get_cluster_id.py
+# import json,sys,os
+# services=json.load(sys.stdin).get("services")
+# for service in services:
+#     if service.get("name") == os.getenv("CDE_CLUSTER_NAME"):
+#         print(service.get("clusterId"))
+# EOF
+#     CLUSTER_ID=$(cdp de list-services | python get_cluster_id.py)
+#     [ ! -z "$CLUSTER_ID" ] && cdp de disable-service --cluster-id $CLUSTER_ID
+# fi
 
 while true; 
 do
@@ -29,6 +29,10 @@ do
             cdp de list-services | egrep ".*?name.*$CDE_CLUSTER_NAME.*" > /dev/null 2>&1 || exit 0
         ;;
         
+        # CDE_VC)
+        #     cdp de list-vcs --cluster-id $CLUSTER_ID | egrep ".*?cdp-devo-lab-env01-cde02-vc01.*" | egrep ".*?name.*$CDE_CLUSTER_NAME.*" > /dev/null 2>&1 || exit 0
+        # ;;
+
         ENV)
             cdp environments describe-environment --environment-name $CDP_ENV_NAME > /dev/null 2>&1 || exit 0
         ;;
