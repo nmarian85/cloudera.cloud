de9f577383288dd6fe7b989ce38e023eb3ebc6d0
Nicolaie.Marian <nicolaie.marian@ecb.europa.eu>
2021-08-15 11:50:00 +0200

    added delete vc cde

--- scripts/wait_for_del.sh
+++ scripts/wait_for_del.sh
@@ -6,18 +6,6 @@ set -o nounset
 
 COMPONENT=$1
 
-# if [ "$COMPONENT" = "CDE" ]; then
-#     cat << EOF > get_cluster_id.py
-# import json,sys,os
-# services=json.load(sys.stdin).get("services")
-# for service in services:
-#     if service.get("name") == os.getenv("CDE_CLUSTER_NAME"):
-#         print(service.get("clusterId"))
-# EOF
-#     CLUSTER_ID=$(cdp de list-services | python get_cluster_id.py)
-#     [ ! -z "$CLUSTER_ID" ] && cdp de disable-service --cluster-id $CLUSTER_ID
-# fi
-
 while true; 
 do
     case $COMPONENT in
