48329d782daef5af95c2e907a6374e6aa9b6ca62
Nicolaie.Marian <nicolaie.marian@ecb.europa.eu>
2021-08-27 17:28:36 +0200

    switching to python

--- env_mgmt.py
+++ env_mgmt.py
@@ -11,14 +11,16 @@ import json
 import os
 from utils import show_progress
 
+
 def dump_install_json(cdp_env_name, cdp_env_info, env_json_skel):
     cdp_env_json = dict(env_json_skel)
     del cdp_env_json["networkCidr"]
     del cdp_env_json["image"]
 
     cdp_env_json["environmentName"] = cdp_env_name
-    credentials = cdp_env_info["credentials"]
-    cdp_env_json["credentialName"] = credentials["cross_account_all_perm"]["credential_name"]
+    cdp_env_json["credentialName"] = cdp_env_info["credentials"]["cross_account_all_perm"][
+        "credential_name"
+    ]
     cdp_env_json["region"] = "eu-central-1"
     cdp_env_json["subnetIds"] = cdp_env_info["subnets"]
     cdp_env_json["vpcId"] = cdp_env_info["vpc_id"]
@@ -26,13 +28,14 @@ def dump_install_json(cdp_env_name, cdp_env_info, env_json_skel):
     cdp_env_json["tags"] = cdp_env_info["tags"]
     cdp_env_json["securityAccess"] = {
         "securityGroupIdForKnox": cdp_env_info["sg"],
-        "defaultSecurityGroupId": cdp_env_info["sg"]
+        "defaultSecurityGroupId": cdp_env_info["sg"],
     }
     cdp_env_json["logStorage"]["storageLocationBase"] = f'{cdp_env_info["log_bucket"]}'
     log_instance_profile = f'{cdp_env_info["log_role"]}-instance-profile'
     role_iam_arn = f'arn:aws:iam::{cdp_env_info["account_id"]}'
-    cdp_env_json["logStorage"]["instanceProfile"] = (
-        f'{role_iam_arn}:instance-profile/{log_instance_profile}')
+    cdp_env_json["logStorage"][
+        "instanceProfile"
+    ] = f"{role_iam_arn}:instance-profile/{log_instance_profile}"
     cdp_env_json["freeIpa"]["instanceCountByGroup"] = 1
     cdp_env_json["endpointAccessGatewayScheme"] = "PRIVATE"
     cdp_env_json["authentication"]["publicKey"] = cdp_env_info["public_key"]
@@ -41,15 +44,19 @@ def dump_install_json(cdp_env_name, cdp_env_info, env_json_skel):
 
 @click.command()
 @click.option("--dryrun/--no-dryrun", default=True)
-@click.option("--action", type=click.Choice(['install-env', 'delete-env']), required=True)
-@click.option("--env", help="ECB environment: lab, test, etc.", required=True)
-@click.option("--cdp-env-name", help="Please see {env}.json file where you defined the CDP env name", required=True)
-def main(
-    dryrun,
-    env,
-    cdp_env_name,
-    action
-):
+@click.option("--action", type=click.Choice(["install-env", "delete-env"]), required=True)
+@click.option(
+    "--env",
+    type=click.Choice(["lab", "test", "acc", "prod"]),
+    help="ECB environment: lab, test, etc.",
+    required=True,
+)
+@click.option(
+    "--cdp-env-name",
+    help="Please see {env}.json file where you defined the CDP env name",
+    required=True,
+)
+def main(dryrun, env, cdp_env_name, action):
     if dryrun:
         show_progress("This is a dryrun")
 
@@ -69,8 +76,9 @@ def main(
     click.echo("Generated JSON")
     click.echo(json.dumps(cdp_env_json, indent=4, sort_keys=True))
 
-    with open(f'{cdp_env_name}.json', "w", encoding="utf-8") as f:
+    with open(f"{cdp_env_name}.json", "w", encoding="utf-8") as f:
         json.dump(cdp_env_json, f, ensure_ascii=False, indent=4)
+
 
 if __name__ == "__main__":
     main()
