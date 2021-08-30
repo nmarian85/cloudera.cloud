51432a9ea7a1866ce3ddb3e3499de49192a6db80
Nicolaie.Marian <nicolaie.marian@ecb.europa.eu>
2021-08-30 11:23:41 +0200

    env create

--- env_mgmt.py
+++ env_mgmt.py
@@ -9,7 +9,7 @@ import click
 import sys
 import json
 import os
-from utils import show_progress, get_env_info, sleep_wait
+from utils import show_progress, get_env_info, poll_for_status
 from cdpv1sign import generate_headers
 import requests_ops
 import requests
@@ -25,7 +25,7 @@ Env variables:
 """
 
 
-def dump_install_json(cdp_env_name, cdp_env_info, env_json_skel):
+def dump_env_install_json(cdp_env_name, cdp_env_info, env_json_skel):
     cdp_env_json = dict(env_json_skel)
     del cdp_env_json["networkCidr"]
     del cdp_env_json["image"]
@@ -55,38 +55,10 @@ def dump_install_json(cdp_env_name, cdp_env_info, env_json_skel):
     return cdp_env_json
 
 
-@sleep_wait
-def poll_for_status(poll_url, action, expected_value):
-    """Since the http request we send is async we will need to poll
-    to check that the action we submitted in the http request was
-    successful
-
-    Args:
-        poll_url ([type]): [url to check the status of our command (e.g. creating a credential)]
-        action ([type]): []
-        expected_value ([type]): [expected value in case of success]
-
-    Returns:
-        [type]: [description]
-    """
-    json_response = requests_ops.send_http_request(
-        srv_url=poll_url, req_type="post", data={}, headers=generate_headers("POST", poll_url),
-    )
-    if action == "install-env":
-        found = False
-    elif action == "delete-env":
-        found = True
-
-    creds = json_response.get("environments")
-    if isinstance(creds, list):
-        for cred in creds:
-            if cred["environmentName"] == expected_value:
-                # if we want to create the credential and we found it,
-                # the return value will be True since the creation was successful
-                # if we wanted to delete the credential and we found it,
-                # the return value will be False since it was not deleted yet
-                return not found
-    return found
+def dump_env_delete_json(cdp_env_name, cdp_env_info, env_json_skel):
+    cdp_env_json = dict(env_json_skel)
+    cdp_env_json["environmentName"] = cdp_env_name
+    return cdp_env_json
 
 
 @click.command()
@@ -112,6 +84,8 @@ def main(dryrun, env, cdp_env_name, action, json_skel):
     if dryrun:
         show_progress("This is a dryrun")
 
+    requests_ops.dryrun = dryrun
+
     with open(json_skel) as json_file:
         env_json_skel = json.load(json_file)
 
@@ -119,76 +93,58 @@ def main(dryrun, env, cdp_env_name, action, json_skel):
     env_url = f"{requests_ops.CDP_SERVICES_ENDPOINT}/environments2"
 
     if action == "install-env":
-        cdp_env_json = dump_install_json(cdp_env_name, cdp_env_info, env_json_skel)
+        click.echo(f"==============Creating environment {cdp_env_name}==============")
+        env_json = dump_env_install_json(cdp_env_name, cdp_env_info, env_json_skel)
         action_url = f"{env_url}/createAWSEnvironment"
-
-    click.echo("-------------------Generated JSON-----------------------------")
-    click.echo(json.dumps(cdp_env_json, indent=4, sort_keys=True))
-
-    if not dryrun:
-        # dumping to a file so that we have evidence which will be stored by the Gitlab pipeline
-        with open(f"{cdp_env_name}.json", "w", encoding="utf-8") as f:
-            json.dump(cdp_env_json, f, ensure_ascii=False, indent=4)
-
-        env_url = f"{requests_ops.CDP_SERVICES_ENDPOINT}/environments2"
-        return requests_ops.send_http_request(
-            srv_url=action_url,
-            req_type="post",
-            data=cdp_env_json,
-            headers=generate_headers("POST", action_url),
-        )
-
-    requests_ops.dryrun = dryrun
-
-    with open(json_skel) as json_file:
-        cred_json_skel = json.load(json_file)
-
-    cdp_env_info = get_env_info(env, cdp_env_name)
-    env_url = f"{requests_ops.CDP_SERVICES_ENDPOINT}/environments2"
-
-    credentials = cdp_env_info["credentials"]
-    for cred, cred_info in credentials.items():
-        cred_name = cred_info["credential_name"]
-
-        if action == "create-cred":
-            click.echo(f"==============Creating credential {cred_name}==============")
-            cdp_cred_json = dump_create_cred_json(cred_info, cred_json_skel)
-            action_url = f"{env_url}/createAWSCredential"
-        elif action == "delete-cred":
-            click.echo(f"==============Deleting credential {cred_name}==============")
-            cdp_cred_json = dump_delete_cred_json(cred_info, cred_json_skel)
-            action_url = f"{env_url}/deleteCredential"
+    elif action == "delete-env":
+        click.echo(f"==============Deleting environment {cdp_env_name}==============")
+        # env_json = dump_env_delete_json(cdp_env_name, cdp_env_info, env_json_skel)
+        action_url = f"{env_url}/deleteEnvironment"
 
         click.echo("-------------------Generated JSON-----------------------------")
-        click.echo(json.dumps(cdp_cred_json, indent=4, sort_keys=True))
+        click.echo(json.dumps(env_json, indent=4, sort_keys=True))
         click.echo("--------------------------------------------------------------")
 
-        if not dryrun:
-            try:
-                response = requests_ops.send_http_request(
-                    srv_url=action_url,
-                    req_type="post",
-                    data=cdp_cred_json,
-                    headers=generate_headers("POST", action_url),
-                )
-            except requests.exceptions.HTTPError:
-                if action == "create-cred":
-                    check_str = "Credential already exists"
-                # we want to ensure an idempotent execution hence
-                # we will not raise errors if the credentials already exist
-                # or where already deleted
-                if check_str not in json.dumps(response, indent=4, sort_keys=True):
-                    raise
-            else:
-                click.echo(f"Waiting for {action} on credential {cred_name}")
-                poll_for_status(
-                    poll_url=f"{env_url}/listCredentials", action=action, expected_value=cred_name,
-                )
-                # dumping file so that Gitlab will back it up
-                with open(f"{cred_name}.json", "w", encoding="utf-8") as f:
-                    json.dump(cdp_cred_json, f, ensure_ascii=False, indent=4)
-        click.echo(f"===========================================================")
-        click.echo()
+    if not dryrun:
+        try:
+            response = requests_ops.send_http_request(
+                srv_url=action_url,
+                req_type="post",
+                data=env_json,
+                headers=generate_headers("POST", action_url),
+            )
+        except requests.exceptions.HTTPError:
+            if action == "install-env":
+                check_str = "Environment already exists"
+            elif action == "delete-env":
+                check_str = "Environment with name"
+            # we want to ensure an idempotent execution hence
+            # we will not raise errors if the environment already exists
+            # or was already deleted
+            if check_str not in json.dumps(response, indent=4, sort_keys=True):
+                raise
+        else:
+            click.echo(f"Waiting for {action} on environments {cdp_env_name}")
+            if action == "install-env":
+                elem_present = True
+            elif action == "delete-env":
+                elem_present = False
+
+            elem_search_info = {
+                "root_index": "environments",
+                "search_elem_index": "environmentName",
+                "present": elem_present,
+                "expected_value": cdp_env_name,
+            }
+
+            poll_for_status(
+                poll_url=f"{env_url}/listEnvironments", elem_search_info=elem_search_info
+            )
+            # dumping file so that Gitlab will back it up
+            with open(f"{cdp_env_name}.json", "w", encoding="utf-8") as f:
+                json.dump(env_json, f, ensure_ascii=False, indent=4)
+    click.echo(f"===========================================================")
+    click.echo()
 
 
 if __name__ == "__main__":
