f8a55681d5813f838e4fe3544aa55aab8518aec9
nicolaie.marian <Nicolaie.Marian@ecb.europa.eu>
2021-11-28 21:55:02 +0100

    testing

--- scripts/dump_aws_auth.py
+++ scripts/dump_aws_auth.py
@@ -8,7 +8,7 @@ del y["metadata"]
 devo_env_name = os.environ["DEVO_ENV_NAME"]
 eks_cluster_type = os.environ["EKS_CLUSTER_TYPE"]
 account_id = os.environ["ACCOUNT_ID"]
-print(yaml.safe_dump(y))
+print(yaml.safe_dump(y, default_flow_style=False, allow_unicode=True))
 # yaml.safe_dump(y["data"]["mapRoles"]
 
 # - rolearn: arn:aws:iam::303413094647:role/jumpserver-role
