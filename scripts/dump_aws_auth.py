b8d82520e4f34c3b890d47cae61ff482199add21
nicolaie.marian <Nicolaie.Marian@ecb.europa.eu>
2021-11-29 10:49:11 +0100

    testing cmap

--- scripts/dump_aws_auth.py
+++ scripts/dump_aws_auth.py
@@ -12,6 +12,18 @@ def str_presenter(dumper, data):
 # yaml.add_representer(str, str_presenter)
 yaml.representer.SafeRepresenter.add_representer(str, str_presenter)
 
+devo_env_name = os.environ["DEVO_ENV_NAME"]
+eks_cluster_type = os.environ["EKS_CLUSTER_TYPE"]
+account_id = os.environ["ACCOUNT_ID"]
+jumprole_arn = f"arn:aws:iam::{account_id}:role/jumpserver-role"
+jumprole_entry = dict(
+    {
+        "rolearn": jumprole_arn,
+        "username": "kubernetes-admin-jumpserver-role",
+        "groups": dict({"system": "masters"}),
+    }
+)
+
 # Load configmap
 aws_auth_yaml = yaml.safe_load(sys.stdin.read())
 del aws_auth_yaml["metadata"]
@@ -23,20 +35,11 @@ del aws_auth_yaml["metadata"]
 #     )
 # )
 map_roles_yaml = yaml.safe_load(aws_auth_yaml["data"]["mapRoles"])
+map_roles_yaml.update(jumprole_entry)
+
 print(yaml.safe_dump(map_roles_yaml, default_flow_style=False, allow_unicode=True))
 
-# devo_env_name = os.environ["DEVO_ENV_NAME"]
-# eks_cluster_type = os.environ["EKS_CLUSTER_TYPE"]
-# account_id = os.environ["ACCOUNT_ID"]
 
-# jumprole_arn = f"arn:aws:iam::{account_id}:role/jumpserver-role"
-# jumprole_entry = dict(
-#     {
-#         "rolearn": jumprole_arn,
-#         "username": "kubernetes-admin-jumpserver-role",
-#         "groups": dict({"system": "masters"}),
-#     }
-# )
 # y["data"]["mapRoles"] = y["data"]["mapRoles"] + yaml.safe_dump(
 #     jumprole_entry, default_flow_style=False, allow_unicode=True
 # )
