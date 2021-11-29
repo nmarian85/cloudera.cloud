e100b1ff6d28191dfa701022dfce8d78bf3a7563
nicolaie.marian <Nicolaie.Marian@ecb.europa.eu>
2021-11-29 10:01:29 +0100

    testing cmap

--- scripts/dump_aws_auth.py
+++ scripts/dump_aws_auth.py
@@ -12,28 +12,31 @@ def str_presenter(dumper, data):
 # yaml.add_representer(str, str_presenter)
 yaml.representer.SafeRepresenter.add_representer(str, str_presenter)
 
+# Load configmap
+aws_auth_yaml = yaml.safe_load(sys.stdin.read())
+del aws_auth_yaml["metadata"]
 
-y = yaml.safe_load(sys.stdin.read())
-del y["metadata"]
-
-devo_env_name = os.environ["DEVO_ENV_NAME"]
-eks_cluster_type = os.environ["EKS_CLUSTER_TYPE"]
-account_id = os.environ["ACCOUNT_ID"]
-
-jumprole_arn = f"arn:aws:iam::{account_id}:role/jumpserver-role"
-jumprole_entry = dict(
-    {
-        "rolearn": jumprole_arn,
-        "username": "kubernetes-admin-jumpserver-role",
-        "groups": dict({"system": "masters"}),
-    },
+# Load multistring value for mapRoles
+map_roles_yaml = yaml.safe_load(
+    yaml.safe_dump(
+        aws_auth_yaml["data"]["mapRoles"], default_flow_style=False, allow_unicode=True
+    )
 )
-y["data"]["mapRoles"] = y["data"]["mapRoles"] + yaml.safe_dump(
-    jumprole_entry, default_flow_style=False, allow_unicode=True
-)
-print(yaml.safe_dump(y, default_flow_style=False, allow_unicode=True))
+print(yaml.safe_dump(map_roles_yaml, default_flow_style=False, allow_unicode=True))
 
-# out_file = f"aws_auth_{devo_env_name}-{eks_cluster_type}.yaml"
-# with open(f"{out_file}", "w", encoding="utf-8") as f:
-#     yaml.safe_dump(y, out_file, default_flow_style=False, allow_unicode=True)
+# devo_env_name = os.environ["DEVO_ENV_NAME"]
+# eks_cluster_type = os.environ["EKS_CLUSTER_TYPE"]
+# account_id = os.environ["ACCOUNT_ID"]
 
+# jumprole_arn = f"arn:aws:iam::{account_id}:role/jumpserver-role"
+# jumprole_entry = dict(
+#     {
+#         "rolearn": jumprole_arn,
+#         "username": "kubernetes-admin-jumpserver-role",
+#         "groups": dict({"system": "masters"}),
+#     }
+# )
+# y["data"]["mapRoles"] = y["data"]["mapRoles"] + yaml.safe_dump(
+#     jumprole_entry, default_flow_style=False, allow_unicode=True
+# )
+# print(yaml.safe_dump(y, default_flow_style=False, allow_unicode=True))
